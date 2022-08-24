d2efda5b25e82c0f94d90d98048f2d3c15424644
iris.campanella <iricam@on.sinch.com>
2022-08-24 20:41:14 -0300

    codigo speech finalizado

--- getVoice.py
+++ getVoice.py
@@ -1,3 +1,5 @@
+import nltk
+nltk.download('punkt')
 import os
 import time
 import playsound
@@ -17,22 +19,64 @@ def get_audio():
     r = sr.Recognizer()
     mic = sr.Microphone()
     audio= ''
+    print("vai entrar no WITH")
     with mic as source:
-        audio = r.listen(source,  phrase_time_limit = 0)
-        said = ""
         print("entrou")
+#        audio = r.listen(source,  phrase_time_limit = 0)
+        r.adjust_for_ambient_noise(source, duration = 1)
+        said = ""
+        print("ajustou ambiente")
+        audio = r.listen(source)
         try:
-#            time.sleep(1.5)
-            print("digaa")
-            said = r.recognize_google(audio)
+            print("reconhecimento")
+            said = r.recognize_google(audio, language="pt-BR")
             print(said)
         except Exception as e:
             print("error: "+str(e))
+
+    tokens = nltk.word_tokenize(str(said))
     
-    return said
+    return tokens
+
+def verificaAndar(tokens):
+
+    buscaUm = -1
+    for i in tokens:
+        if i == "oitavo" or i == "oito" or i == "topo":
+            buscaUm = 8
+        if i == "sétimo" or i == "setimo" or i == "sete":
+            buscaUm = 7
+        if i == "sexto" or i == "seis":
+            buscaUm = 6
+        if i == "quinto" or i == "cinco":
+            buscaUm = 5
+        elif i == "quatro" or i == "quatro":
+            buscaNum = 4
+        elif i == "terceiro" or i == "três":
+            buscaUm = 3
+        elif i == "segundo" or i == "dois":
+            buscaUm == 2
+        elif i == "primeiro" or i == "um":
+            buscaUm = 1
+        elif i == "terreo" or i == "zero" or i == "térreo" or i == "piso vermelho":
+            buscaUm = 0
+    
+    if buscaUm == -1:
+        speak("Não entendi o andar, fale novamente, por favor")
+        frase = get_audio()
+        buscaUm = verificaAndar(frase)    
+    else:
+        speak("Beleza, vamos para o " + str(buscaUm))
+        return buscaUm
 
 speak("Olá, para onde quer ir?!")
 for mic in sr.Microphone.list_microphone_names():
     print(mic)
 
-get_audio()
+frase = get_audio()
+
+busca = verificaAndar(frase)
+#if busca == -1:
+ #   speak("Não entendi o andar, fale novamente, por favor")
+  #  frase = get_audio()
+   # busca = verificaAndar(frase)
